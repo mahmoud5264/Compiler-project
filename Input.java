691b27e9a6b90e1383bf2c4a70fc062978c2fd12
mahmoudatia <mahmoud.atia@bld.ai>
2023-03-21 00:17:24 +0200

    Test another example

--- Input.java
+++ Input.java
@@ -1,13 +1,16 @@
-import org.antlr.v4.runtime.TokenStreamRewriter;
-
-public class TempJavaListener extends JavaParserBaseListener {
-    TokenStreamRewriter rewriter;
-    int blockNumber;
-
-    public TempJavaListener(TokenStreamRewriter rewriter) {
-        this.rewriter = rewriter;
-        this.blockNumber = 0;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        int a=3;
+        int b=4;
+        if(a<b){
+            System.out.println("a is less than b");
+        }
+        else if(a==b){
+            System.out.println("a is equal to b");
+        }
+        else{
+            System.out.println("a is bigger than b");
+        }
     }
-
 
 }
--- Output.java
+++ Output.java
@@ -1,30 +1,16 @@
-import org.antlr.v4.runtime.TokenStreamRewriter;
-
-public class TempJavaListener extends JavaParserBaseListener {
-    TokenStreamRewriter rewriter;
-    int blockNumber;
-
-    public TempJavaListener(TokenStreamRewriter rewriter) {//block number 1
-        this.rewriter = rewriter;
-        this.blockNumber = 0;
-
-        if(true){//block number 2
-            if(false){//block number 3
-
-            }
-
-            else{//block number 4
-
-            }
+public class Main {
+    public static void main(String[] args) throws Exception {//block number 1
+        int a=3;
+        int b=4;
+        if(a<b){//block number 2
+            System.out.println("a is less than b");
         }
-        else{//block number 5
-
+        else if(a==b){//block number 3
+            System.out.println("a is equal to b");
+        }
+        else{//block number 4
+            System.out.println("a is bigger than b");
         }
     }
 
-    @Override
-    public void enterBlock(JavaParser.BlockContext ctx) {//block number 6
-        this.blockNumber++;
-        rewriter.insertAfter(ctx.getStart(),"//block number " + this.blockNumber);
-    }
 }
