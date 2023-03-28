633512bff9559a3487485caa0d20908ca8691f2c
Muhamed_Abdelghany <66431030+MuhamedAbdelghany@users.noreply.github.com>
2023-03-28 17:21:10 +0200

    Update Input.java

--- Input.java
+++ Input.java
@@ -1,15 +1,33 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+
 public class Main {
     public static void main(String[] args) throws Exception {
         int a=3;
         int b=4;
+
         if(a<b){
             System.out.println("a is less than b");
+            for(int i=0;i<2;i++){
+
+            }
         }
         else if(a==b){
             System.out.println("a is equal to b");
+            for(int i=0;i<2;i++){
+
+            }
         }
         else{
+
             System.out.println("a is bigger than b");
+            for(int i=0;i<3;i++){
+
+            }
+        }
+        for(int i=0;i<4;i++){
+
         }
     }
 
