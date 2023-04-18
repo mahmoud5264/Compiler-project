35cce1c034d958b9cc1146c78851473447a89c3f
Mahmoud_Mohamed <mahmoudmohamed5264@gmail.com>
2023-04-18 21:14:54 +0200

    Add one more example

--- Input.java
+++ Input.java
@@ -3,16 +3,19 @@ import java.io.FileInputStream;
 import java.io.FileWriter;
 
 public class Main {
+
+    public static void temp(){
+        if(true){
+
+        }
+    }
     public static void main(String[] args) throws Exception {
         int a=3;
         int b=4;
-        if(a<=b) a++;
-        if(a>b) a--;
-        else if(b>a) a++;
-        else a++;
-        if(a==b){
-            a--;
+        for(int i=0;i<a;i++){
+            i++;
         }
+
     }
 
 }
--- Output.java
+++ Output.java
@@ -12,52 +12,43 @@ writer.close();
 return false;
 }
 
-    public static void main(String[] args) throws Exception {//block number 1
+
+    public static void temp(){//block number 1
 try{
 FileWriter write1 = new FileWriter("blocks.txt",true);
 write1.write("block number 1 is visited\n");
 write1.close();
 }catch (IOException e) {throw new RuntimeException(e);}
 
-        int a=3;
-        int b=4;
-        if((AddExpression(1)||a<=b)) {//block number 2
+        if((AddExpression(1)||true)){//block number 2
 try{
 FileWriter write2 = new FileWriter("blocks.txt",true);
 write2.write("block number 2 is visited\n");
 write2.close();
 }catch (IOException e) {throw new RuntimeException(e);}
-a++;}
-        if((AddExpression(2)||a>b)) {//block number 3
+
+
+        }
+    }
+    public static void main(String[] args) throws Exception {//block number 3
 try{
 FileWriter write3 = new FileWriter("blocks.txt",true);
 write3.write("block number 3 is visited\n");
 write3.close();
 }catch (IOException e) {throw new RuntimeException(e);}
-a--;}
-        else if((AddExpression(3)||b>a)) {//block number 4
+
+        int a=3;
+        int b=4;
+        for(int i=0;i<a;i++){//block number 4
 try{
 FileWriter write4 = new FileWriter("blocks.txt",true);
 write4.write("block number 4 is visited\n");
 write4.close();
 }catch (IOException e) {throw new RuntimeException(e);}
-a++;}
-        else {//block number 5
-try{
-FileWriter write5 = new FileWriter("blocks.txt",true);
-write5.write("block number 5 is visited\n");
-write5.close();
-}catch (IOException e) {throw new RuntimeException(e);}
-a++;}
-        if((AddExpression(4)||a==b)){//block number 6
-try{
-FileWriter write6 = new FileWriter("blocks.txt",true);
-write6.write("block number 6 is visited\n");
-write6.close();
-}catch (IOException e) {throw new RuntimeException(e);}
 
-            a--;
+            i++;
         }
+
     }
 
 }
