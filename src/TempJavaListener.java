679169bb5bf94be868452cd4aabf63e4910d8778
yasminahmed01 <65883164+yasminahmed01@users.noreply.github.com>
2023-04-18 04:41:08 +0200

    Update TempJavaListener.java

--- src/TempJavaListener.java
+++ src/TempJavaListener.java
@@ -1,5 +1,9 @@
 import org.antlr.v4.runtime.TokenStreamRewriter;
 
+
+public void enterCompilationUnit(JavaParser.CompilationUnitContext ctx) {
+        rewriter.insertBefore(ctx.getStart(), "import java.io.*;\n");
+    }
 public class TempJavaListener extends JavaParserBaseListener {
     TokenStreamRewriter rewriter;
     int blockNumber;
@@ -9,13 +13,15 @@ public class TempJavaListener extends JavaParserBaseListener {
         this.blockNumber = 0;
     }
 
-    @Override
+ 
+@Override
     public void enterBlock(JavaParser.BlockContext ctx) {
         this.blockNumber++;
-        rewriter.insertAfter(ctx.getStart(),"//block number " + this.blockNumber+"\n");
-        rewriter.insertAfter(ctx.getStart(),"FileWriter write"+this.blockNumber+" = new FileWriter(\"Output.txt\",true);\n");
-        rewriter.insertAfter(ctx.getStart(),"write"+this.blockNumber+".write(\"block number "+this.blockNumber+" is visited\\n\");\n");
-        rewriter.insertAfter(ctx.getStart(),"write"+this.blockNumber+".close();\n");
-   
+        rewriter.insertAfter(ctx.getStart(), "//block number " + this.blockNumber + "\n");
+        rewriter.insertAfter(ctx.getStart(), "try{\n");
+        rewriter.insertAfter(ctx.getStart(), "FileWriter write" + this.blockNumber + " = new FileWriter(\"blocks.txt\",true);\n");
+        rewriter.insertAfter(ctx.getStart(), "write" + this.blockNumber + ".write(\"block number " + this.blockNumber + " is visited\\n\");\n");
+        rewriter.insertAfter(ctx.getStart(), "write" + this.blockNumber + ".close();\n");
+        rewriter.insertAfter(ctx.getStart(), "}catch (IOException e) {throw new RuntimeException(e);}\n");
     }
 }
