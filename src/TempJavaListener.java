49ad6c63643eb12e8d228416ec47a12b5f47d8a4
AmrMohamed <66434864+Amr-al@users.noreply.github.com>
2023-04-18 18:17:02 +0200

    Update TempJavaListener.java

--- src/TempJavaListener.java
+++ src/TempJavaListener.java
@@ -1,20 +1,23 @@
 import org.antlr.v4.runtime.TokenStreamRewriter;
+import org.antlr.v4.runtime.tree.TerminalNode;
 
+import java.io.*;
 
-public void enterCompilationUnit(JavaParser.CompilationUnitContext ctx) {
-        rewriter.insertBefore(ctx.getStart(), "import java.io.*;\n");
-    }
 public class TempJavaListener extends JavaParserBaseListener {
     TokenStreamRewriter rewriter;
     int blockNumber;
+    int expressionNumber;
+    boolean isInside;
 
     public TempJavaListener(TokenStreamRewriter rewriter) {
         this.rewriter = rewriter;
         this.blockNumber = 0;
+        this.expressionNumber = 0;
+        this.isInside = false;
     }
 
- 
-@Override
+
+    @Override
     public void enterBlock(JavaParser.BlockContext ctx) {
         this.blockNumber++;
         rewriter.insertAfter(ctx.getStart(), "//block number " + this.blockNumber + "\n");
@@ -24,4 +27,46 @@ public class TempJavaListener extends JavaParserBaseListener {
         rewriter.insertAfter(ctx.getStart(), "write" + this.blockNumber + ".close();\n");
         rewriter.insertAfter(ctx.getStart(), "}catch (IOException e) {throw new RuntimeException(e);}\n");
     }
+
+    @Override
+    public void enterCompilationUnit(JavaParser.CompilationUnitContext ctx) {
+        rewriter.insertBefore(ctx.getStart(), "import java.io.*;\n");
+    }
+
+    @Override
+    public void enterClassBody(JavaParser.ClassBodyContext ctx) {
+        rewriter.insertAfter(ctx.getStart(), "public static boolean AddExpression(int currentNumber) {\n" +
+                "try{\n" +
+                "FileWriter writer = new FileWriter(\"expressions.txt\",true);\n" +
+                "writer.write(\"expression number \"+currentNumber+\" is visited\\n\");\n" +
+                "writer.close();\n}" +
+                "catch(Exception e){}\n" +
+                "return false;\n" +
+                "}\n");
+    }
+
+    @Override
+    public void visitTerminal(TerminalNode node) {
+        if (node.getText().equals("||") || node.getText().equals("&&")) {
+            isInside = true;
+        }
+    }
+
+
+    @Override
+    public void enterExpression(JavaParser.ExpressionContext ctx) {
+        if (isInside && ctx.AND() == null && ctx.OR() == null) {
+            expressionNumber++;
+            rewriter.insertBefore(ctx.getStart(), "(AddExpression(" + expressionNumber + ")||");
+            rewriter.insertAfter(ctx.getStop(), ")");
+            isInside = false;
+            if (ctx.getText().charAt(0) == '(') isInside = true;
+        }
+    }
+    @Override
+    public void enterParExpression(JavaParser.ParExpressionContext ctx) {
+        isInside = true;
+    }
+
+
 }
