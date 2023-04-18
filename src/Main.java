3b7461950287acb6ac33327165a1bb7a065bc8b0
AmrMohamed <66434864+Amr-al@users.noreply.github.com>
2023-04-18 19:25:06 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileWriter;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         String inputFile="Input.java";
         FileInputStream filestream=new FileInputStream(inputFile);
         ANTLRInputStream input=new ANTLRInputStream(filestream);
@@ -16,12 +14,65 @@ public class Main {
         ParseTree tree = parser.compilationUnit();
         TokenStreamRewriter rewriter = new TokenStreamRewriter(tokens);
         ParseTreeWalker walker = new ParseTreeWalker();
-        walker.walk(new TempJavaListener(rewriter), tree);
-        File output = new File("Output.java");
+        walker.walk(new BlocksJavaListener(rewriter), tree);
+        File output = new File("Updated.java");
         output.createNewFile();
-        FileWriter write = new FileWriter("Output.java");
+        FileWriter write = new FileWriter("Updated.java");
         write.write(rewriter.getText());
         write.close();
+
+        inputFile="Updated.java";
+        filestream=new FileInputStream(inputFile);
+        input=new ANTLRInputStream(filestream);
+        lexer=new JavaLexer(input);
+        tokens= new CommonTokenStream(lexer);
+        parser = new JavaParser(tokens);
+        tree = parser.compilationUnit();
+        rewriter = new TokenStreamRewriter(tokens);
+        walker = new ParseTreeWalker();
+        walker.walk(new TempJavaListener(rewriter), tree);
+        output = new File("Output.java");
+        output.createNewFile();
+        write = new FileWriter("Output.java");
+        write.write(rewriter.getText());
+        write.close();
+
+
+        Runtime rt = Runtime.getRuntime();
+        Process pr = rt.exec("java Output.java");
+        try {
+            pr.waitFor();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        inputFile="Updated.java";
+        filestream=new FileInputStream(inputFile);
+        input=new ANTLRInputStream(filestream);
+        lexer=new JavaLexer(input);
+        tokens= new CommonTokenStream(lexer);
+        parser = new JavaParser(tokens);
+        tree = parser.compilationUnit();
+        rewriter = new TokenStreamRewriter(tokens);
+        for (int i = 0; i < tokens.getTokens().size(); i++) {
+            Token token = tokens.getTokens().get(i);
+            if (token.getText().equals("<")) {
+                rewriter.replace(token, "&lt;");
+            }
+            else if (token.getText().equals(">")) {
+                rewriter.replace(token, "&gt;");
+            }
+        }
+
+        walker = new ParseTreeWalker();
+        walker.walk(new TempJavaListener2(rewriter), tree);
+        output = new File("final.html");
+        output.createNewFile();
+        write = new FileWriter("final.html");
+        write.write(rewriter.getText());
+        write.close();
+
     }
 
 }
